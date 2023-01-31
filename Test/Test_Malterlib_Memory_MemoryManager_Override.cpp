19632e0f5b79d259ee8da24b9f3eed9c18fa7bf4
Erik Olofsson <erik@olofsson.info>
2023-02-01 00:19:37 +0100

    [Malterlib/Memory] Fix bugs related to macOS try size
    Assume safe try size is needed even when no non-default malloc zone has been created
    Fix communication with exception server being broken on macOS Ventura for arm64
    Fix potential race uninstalling longjump when enabling exception server
    Only enable exception server when being debugged (longjump is faster)
    Add tests for try size

---
+++ Test/Test_Malterlib_Memory_MemoryManager_Override.cpp
@@ -0,0 +1,124 @@
+// Copyright © 2023 Favro Holding AB
+// Distributed under the MIT license, see license text in LICENSE.Malterlib
+
+#include <Mib/Test/Test>
+#include <Mib/Test/Performance>
+
+#ifdef DPlatformFamily_macOS
+	#include <memory>
+	#include <malloc/malloc.h>
+#endif
+
+namespace
+{
+	using namespace NMib;
+	using namespace NMib::NMemory;
+	using namespace NMib::NStr;
+	using namespace NMib::NContainer;
+
+	class COverride_Tests : public CTest
+	{
+	public:
+
+		void f_DoTests()
+		{
+#ifdef DPlatformFamily_macOS
+			DMibTestSuite("TrySize")
+			{
+				void *pMemory = malloc(5);
+				auto Cleanup = g_OnScopeExit / [&]
+					{
+						free(pMemory);
+					}
+				;
+				auto Size = malloc_size(pMemory);
+				DMibExpect(Size, >, 0);
+
+				auto Size2 = malloc_size(&pMemory);
+				DMibExpect(Size2, ==, 0);
+
+				auto *pInvalidMemory = (void* *)(mint)4096;
+
+				auto SizeInvalidMemory = malloc_size(pInvalidMemory);
+				DMibExpect(SizeInvalidMemory, ==, 0);
+			};
+			DMibTestSuite(CTestCategory("TrySizePerf") << CTestGroup("Performance"))
+			{
+				auto fDoTest = [](mint _nZones)
+					{
+						DMibTestPath("{} Zones"_f << _nZones);
+
+						TCVector<malloc_zone_t *> Zones;
+
+						for (mint i = 0; i < _nZones; ++i)
+							Zones.f_Insert(malloc_create_zone(0, 0));
+
+						void *pMemory;
+						if (_nZones)
+							pMemory = malloc_zone_malloc(Zones.f_GetLast(), 5);
+						else
+							pMemory = malloc(5);
+
+						auto Cleanup = g_OnScopeExit / [&]
+							{
+								free(pMemory);
+
+								for (auto &pZone : Zones)
+									malloc_destroy_zone(pZone);
+							}
+						;
+
+
+						{
+							constexpr mint c_nTests = 5;
+							constexpr mint c_nSizeTests = 10000;
+
+							CTestPerformanceMeasure MallocSizeTime("malloc_size");
+							for (mint i = 0; i < c_nTests; ++i)
+							{
+								DMibTestScopeMeasure(MallocSizeTime, c_nSizeTests);
+								for (mint i = 0; i < c_nSizeTests; ++i)
+								{
+									auto *pInvalidMemory = (void* *)(mint)4096;
+									[[maybe_unused]] auto SizeInvalidMemory = malloc_size(pInvalidMemory);
+								}
+							}
+
+							CTestPerformance SadCase(1.0);
+							SadCase.f_Add(MallocSizeTime);
+
+							DMibTest(DMibExpr(SadCase));
+						}
+						{
+							constexpr mint c_nTests = 5;
+							constexpr mint c_nSizeTests = 10000;
+
+							CTestPerformanceMeasure MallocSizeTime("malloc_size");
+
+							for (mint i = 0; i < c_nTests; ++i)
+							{
+								DMibTestScopeMeasure(MallocSizeTime, c_nSizeTests);
+								for (mint i = 0; i < c_nSizeTests; ++i)
+								{
+									[[maybe_unused]] auto SizeInvalidMemory = malloc_size(pMemory);
+								}
+							}
+
+							CTestPerformance HappyCase(1.0);
+							HappyCase.f_Add(MallocSizeTime);
+
+							DMibTest(DMibExpr(HappyCase));
+						}
+					}
+				;
+				fDoTest(0);
+				fDoTest(10);
+				fDoTest(100);
+				fDoTest(200);
+			};
+#endif
+		}
+	};
+
+	DMibTestRegister(COverride_Tests, Malterlib::Memory::MemoryManager);
+}
