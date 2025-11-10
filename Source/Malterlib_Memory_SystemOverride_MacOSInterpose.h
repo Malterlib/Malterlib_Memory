589996b375680c6df88ce2f4b2c0fd32774bb2c6
Erik Olofsson <erik@olofsson.info>
2025-11-10 12:55:45 +0100

    [Malterlib/Memory] Fix compile on older Xcode SDKs

--- Source/Malterlib_Memory_SystemOverride_MacOSInterpose.h
+++ Source/Malterlib_Memory_SystemOverride_MacOSInterpose.h
@@ -1,4 +1,4 @@
-// Copyright © 2015 Hansoft AB 
+// Copyright © 2015 Hansoft AB
 // Distributed under the MIT license, see license text in LICENSE.Malterlib
 
 typedef unsigned long long malloc_type_id_t;
@@ -48,6 +48,12 @@ extern "C" void _ZdlPvmSt11align_val_t(void *, size_t, size_t) __attribute__((we
 extern "C" void vfree(void *) __attribute__((weak_import));
 
 using malloc_options_np_t = uint64_t;
+#if __MAC_OS_X_VERSION_MAX_ALLOWED < 260000
+	using malloc_zone_malloc_options_t = uint64_t;
+
+	extern "C" void * malloc_zone_malloc_with_options(malloc_zone_t *zone, size_t align, size_t size, malloc_zone_malloc_options_t opts);
+	extern "C" void * malloc_type_zone_malloc_with_options(malloc_zone_t *zone, size_t alignment, size_t size, malloc_type_id_t type_id, malloc_zone_malloc_options_t opts);
+#endif
 
 extern "C" boolean_t malloc_zone_claimed_address(malloc_zone_t *_0, void *_1) __attribute__((weak_import));
 extern "C" void *malloc_zone_malloc_with_options_np(malloc_zone_t *_0, size_t _1, size_t _2, malloc_options_np_t _3) __attribute__((weak_import));
